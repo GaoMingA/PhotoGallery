1d17e83ad243a1e088eade6018ce5caeb37e5ee6
101003111 <gaoming@huaqin.com>
2017-09-06 18:10:22 +0800

    主线程更新下载到的图片

--- app/src/main/java/com/example/gaoming/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/gaoming/photogallery/PhotoGalleryFragment.java
@@ -1,8 +1,11 @@
 package com.example.gaoming.photogallery;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -39,7 +42,17 @@ public class PhotoGalleryFragment extends Fragment {
         setRetainInstance(true);
         new FetchItemsTask().execute();
 
-        mThumbnailDownloader = new ThumbnailDownloader<>();
+        Handler responseHandler = new Handler();
+        mThumbnailDownloader = new ThumbnailDownloader<>(responseHandler);
+        mThumbnailDownloader.setThumbnailDownloadListener(
+                new ThumbnailDownloader.ThumbnailDownloadListener<PhotoHolder>() {
+                    @Override
+                    public void onThumbnailDownloaded(PhotoHolder photoHolder, Bitmap bitmap) {
+                        Drawable drawable = new BitmapDrawable(getResources(), bitmap);
+                        photoHolder.bindDrawable(drawable);
+                    }
+                }
+        );
         mThumbnailDownloader.start();
         mThumbnailDownloader.getLooper();
         Log.i(TAG, "Background thread started");
@@ -54,6 +67,12 @@ public class PhotoGalleryFragment extends Fragment {
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
         setupAdapter();
         return v;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mThumbnailDownloader.clearQueue();
     }
 
     @Override
--- app/src/main/java/com/example/gaoming/photogallery/ThumbnailDownloader.java
+++ app/src/main/java/com/example/gaoming/photogallery/ThumbnailDownloader.java
@@ -22,9 +22,20 @@ public class ThumbnailDownloader<T> extends HandlerThread{
     private Boolean mHasQuit = false;
     private Handler mRequestHandler;
     private ConcurrentMap<T,String> mRequestMap = new ConcurrentHashMap<>();
+    private Handler mResponseHandler;
+    private ThumbnailDownloadListener<T> mThumbnailDownloadListener;
 
-    public ThumbnailDownloader() {
+    public interface ThumbnailDownloadListener<T> {
+        void onThumbnailDownloaded(T target, Bitmap thumbnail);
+    }
+
+    public void setThumbnailDownloadListener(ThumbnailDownloadListener<T> listener) {
+        mThumbnailDownloadListener = listener;
+    }
+
+    public ThumbnailDownloader(Handler responseHandler) {
         super(TAG);
+        mResponseHandler = responseHandler;
     }
 
     @Override
@@ -42,6 +53,10 @@ public class ThumbnailDownloader<T> extends HandlerThread{
                     .sendToTarget();
         }
         Log.i(TAG, "Got a URL: " + url);
+    }
+
+    public void clearQueue() {
+        mRequestHandler.removeMessages(MESSAGE_DOWNLOAD);
     }
 
     @Override
@@ -69,6 +84,17 @@ public class ThumbnailDownloader<T> extends HandlerThread{
             final Bitmap bitmap = BitmapFactory
                     .decodeByteArray(bitmapBytes,0,bitmapBytes.length);
             Log.i(TAG, "Bitmap created");
+
+            mResponseHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if (mRequestMap.get(target) != url || mHasQuit){
+                        return;
+                    }
+                    mRequestMap.remove(target);
+                    mThumbnailDownloadListener.onThumbnailDownloaded(target,bitmap);
+                }
+            });
         } catch (IOException e) {
             Log.e(TAG, "Error downloading image", e);
         }
